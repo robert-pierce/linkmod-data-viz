3eb847cf4c885bd0b888a89d46aae2ef385cbcc8
robert-pierce <rpierce3@gmail.com>
2019-04-08 11:29:22 -0400

    add srp apt vega

--- src/linkmod_data_viz/linkmod-viz.clj
+++ src/linkmod_data_viz/linkmod-viz.clj
@@ -3,19 +3,8 @@
             [org.httpkit.client :as http]
             [oz.server :as server]
             [clojure.java.io :as io]
+            [linkmod-data-viz.Srp-Apt-Top-300 :as Srp-Apt-Vega]
             [clojure.data.csv :as csv]))
-
-(defn play-data [& names]
-  (for [n names
-        i (range 20)]
-    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
-
-(def line-plot
-  {:data {:values (play-data "monkey" "slipper" "broom")}
-   :encoding {:x {:field "time"}
-              :y {:field "quantity"}
-              :color {:field "item" :type "nominal"}}
-   :mark "line"})
 
 (defn read-csv
   [url]
@@ -27,19 +16,3 @@
               (map keyword)
               repeat)
          (rest data))))
-
-(def topo-viz
-  {:$schema "https://vega.github.io/schema/vega-lite/v3.json"
-   :width 900
-   :height 560
-   :projection {:type "albersUsa"}
-   :layer [{:data {:name "states"
-                   :url "https://vega.github.io/vega-tutorials/airports/data/us-10m.json"
-                   :format {:type "topojson"
-                      :feature "states"}}
-            :mark {:type "geoshape"
-                   :fill "lightgray"
-                   :stroke "white"}}
-           {:data {:url "https://gist.github.com/robert-pierce/cc4ba2da18aaaf81cccbb9323e34ccaa"}}
-
-           ]})
